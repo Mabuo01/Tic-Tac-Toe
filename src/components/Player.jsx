d3b81cb941c212cad1d231a6b3b4217f326b65c7
Makoto <ma6ko1to6@gmail.com>
2024-03-12 03:31:53 +0900

    初期化

---
+++ src/components/Player.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol, isActive }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditting, setIsEditting] = useState(false);
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  if (isEditting) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+  }
+
+  function handleEditClick() {
+    setIsEditting((editting) => !editting);
+  }
+
+  return (
+    <li className={isActive ? 'active' : undefined}>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditting ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
